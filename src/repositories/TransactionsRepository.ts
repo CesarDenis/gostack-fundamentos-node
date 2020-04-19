45ecdf1639b67066b042c7af07a9158328015e9f
Cesar Denis <cesar@cesardenis.com>
2020-04-18 22:57:21 -0300

    feat: add challenge resolution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +19,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.type === 'income') {
+          return {
+            ...accumulator,
+            income: accumulator.income + currentValue.value,
+            total: accumulator.total + currentValue.value,
+          };
+        }
+
+        return {
+          ...accumulator,
+          outcome: accumulator.outcome + currentValue.value,
+          total: accumulator.total - currentValue.value,
+        };
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+
+    if (value > total && type === 'outcome') {
+      throw Error('You don`t have enough balance');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
